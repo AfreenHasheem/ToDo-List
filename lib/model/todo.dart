db70dcee7962e4554f07fb82dc32c78734f8a780
Siraj <asrf@C02SG255G8WN.local>
2019-06-13 12:09:34 +0100

    Added additional comments to Todo class

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -5,9 +5,15 @@ class Todo {
   int _priority;
   String _date;
 
+  /*[this.description] is optional parameter hence enclosed in []
+  this in an unnamed constructor*/
   Todo(this._title, this._priority, this._date, [this._description]);
-  //[this.description] is optional parameter hence enclosed in []
 
+
+  /*constructor when ur editing the todoTable which will have an id
+  You can have only 1 unnamed constructor in a class
+  This is a named constructor
+  It works exactly as the unnamed constructor*/
   Todo.withId(this._id, this._title, this._priority, this._date,[this._description]);
 
   String get date => _date;
@@ -47,6 +53,12 @@ class Todo {
     _date = newDate;
   }
 
+  /*
+  Maps are collections of key-value pairs from which you retrieve a value using it associated key.
+  Maps and their keys and value can be iterated.
+  The below method transforms our todoTable into a map
+  This will be handy when we use helper methods for SQLite
+  */
   Map <String, dynamic> toMap(){
     var map = Map<String, dynamic>();
     map["title"] = _title;
@@ -59,6 +71,7 @@ class Todo {
     return map;
   }
 
+  //This method does the opposite of a Map. It takes in any object and transforms it into a todo.
   Todo.fromObject(dynamic o){
     this._id = o["id"];
     this._title = o["title"];
